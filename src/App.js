0a25aceec97ded98b08e893e8f11b524cfeb76ef
Nefelijm <sistemasnik20@gmail.com>
2018-03-21 19:57:29 -0500

    Componente asider

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NivelUno from './container/nivelUno-container'
 import NivelDos from './container/nivelDos-container'
 import NivelTres from './container/nivelTres-container'
 import Escenario from './componentes/escenario'
+import Aside from './componentes/aside'
 // const none = 'block';
 import { Provider } from 'react-redux';
 import store from './reducer/reducer';
@@ -12,6 +13,7 @@ import store from './reducer/reducer';
 const App =() => (
   <Provider store= {store}>
   <div>
+  <Aside/>    
   <Escenario/>
   <NivelUno/>
   <NivelDos/>
---
+++ src/componentes/aside.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import  './aside.css';
+import teatro from '../assets/images/teatro.jpg';
+const Aside = () =>(
+    <div className = "container-fluid">
+       <div className="row">
+          <div className="col-12 text-center">
+             <h3>Teatro - Mucho ruido por nada - Corta temporada</h3>
+             <img src={teatro}/> 
+          </div>
+      </div>
+        <div className="row">
+          <div className="col-12 text-center">
+             <p className = 'aside'>En la época de Shakespeare las mujeres estaban prohibidas de actuar. Hoy, 14 hombres asumen el reto de ponerse en los zapatos de la mujer. A través de sus brillantes personajes, Shakespeare expone las complejidades del amor y nos recuerda la importancia de la comunidad sobre la vida en pareja.</p>
+             <p>Autor: William Shakespeare</p> 
+          </div>
+        </div>
+    </div>
+)
+
+export default Aside
