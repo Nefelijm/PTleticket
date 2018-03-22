75cb745bf728487d5622557c2e62adea6d0c9236
Nefelijm <sistemasnik20@gmail.com>
2018-03-22 01:37:56 -0500

    Creando los estados y definiendo cada propiedades en en containerciculo

--- src/componentes/nivel-uno.js
+++ src/componentes/nivel-uno.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ContainerCirculo from './containerCirculo'
 
-const NivelUno = ({id}) =>{
+const NivelUno = props =>{
 
     //ancho y color del rectangulo
     const width = [0,300,500, 566]
@@ -10,17 +10,22 @@ const NivelUno = ({id}) =>{
     return(
     //coordenadas del rectangulo
      <svg
-       x = {(200/id)}
-       y = {150 * id}
+       x = {(200/props.id)}
+       y = {150 * props.id}
      >
      <rect
-      width = {width[id]}
+      width = {width[props.id]}
       height="150"
-      fill={fill[id]}
+      fill={fill[props.id]}
       />
 
    {/* Creamos contenedor para todo los circulos */}
-      <ContainerCirculo />
+      <ContainerCirculo 
+        selected={props.selected}//propiedad estado
+        nivel={props.id}//cuantos niveles habra
+        display={props.niveles[props.id].display}//para indicar none o block
+        elements={props.niveles[props.id].butacas}//para pintar la cantidad de butacas
+        {/* colsPerRow={parseInt(props.niveles[props.id].butacas, 10)/2}/> //divide a las butacas en 2 para hacer dos filas */}
 
      </svg>
     )
---
+++ src/reducers/reducer.js
@@ -0,0 +1,20 @@
+//Crearemos nuestro estado para poder pasar a nuestras componentes
+const initialState = {
+    niveles:{
+        1: {
+          display:'none', //Esta propiedad permitira poner un display none o block al componente que envuelve a los circulos (containerCirculo)
+          butacas: 10,  //Cantidad de butacas que se creeran
+        }
+        2: {
+           display:'none',
+           butacas: 15, 
+        }
+        3: {
+           display:'none',
+           butacas: 20, 
+        }
+
+    }
+    selected:{}//Este estado cambiara de acuerdo a las butacas que seleccionaremos
+    
+    }
