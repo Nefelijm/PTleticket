9de8f37cf6b2c168063f9e7c1138b5afa2501f07
elenaJO <ele.lucero1993@gmail.com>
2018-04-03 12:42:32 -0500

    Updates

--- src/acciones/accion.js
+++
@@ -1,9 +0,0 @@
-// const DISPLAY_BUTACAS = 'DISPLAY_BUTACAS';
-// const TOGGLE_BUTACA = 'TOGGLE_BUTACA';
-
-
-export const displayButacas = payload => ({ type: 'DISPLAY_BUTACAS', payload });
-export const toggleButaca = payload => ({ type: 'TOGGLE_BUTACA', payload });
-export const comprar = payload => ({ type: 'COMPRAR', payload });
-
-
